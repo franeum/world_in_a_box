44b8c9865f755087ac5aecbf53d9a105807200e4
franeum desk <elettrochuck@gmail.com>
2022-06-20 11:17:04 +0200

    update

--- source/wab_bg.py
+++ source/wab_bg.py
@@ -1,20 +1,28 @@
+"""
+background manager
+"""
+
 import tkinter as tk
 import tkinter.font as tkFont
 import time
 
+
 def draw_bg():
+    """
+    draw background with countdown
+    """
     root = tk.Tk()
     root.configure(bg='black', cursor='none')
     root.attributes('-fullscreen', True)
-    
-    win = tk.Label(root, text="", background="#000", foreground="#fff",
-            font=tkFont.Font(family='Helvetica', size=72, weight='bold'))
 
-    for i in range(3,0,-1):
+    win = tk.Label(root, text="", background="#000", foreground="#fff",
+                   font=tkFont.Font(family='Helvetica', size=72, weight='bold'))
+
+    for i in range(3, 0, -1):
         win['text'] = str(i)
         win.pack(ipadx=10, ipady=300)
         root.update()
-        time.sleep(1)
+        time.sleep(0.75)
 
     win.destroy()
     root.update()
